019bb992cac0cd567cda654169d54ec7dd8f3b32
kucheriavii <kucheriavii@gmail.com>
2015-08-23 23:11:45 +0300

    index.html

--- index.php
+++ index.php
@@ -3,14 +3,31 @@
 <head>
 	<meta charset="UTF-8">
 	<title>test work</title>
-	<link rel="stylesheet" href="main.css">
+	<link rel="stylesheet" href="css/main.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
 </head>
 <body>
 	<!--BOOTSTRAP INIT-->
-	<div class="container-fluid">
-
-	</div>	
+		<div class="container-fluid main">
+			<!--Header and menu block-->
+			<div class="row header">
+				<div class="col-lg-2 logo"></div>
+				<div class="col-lg-9 top-menu">
+					<ul class="menu">
+						<li class="menu-unit">О компании</li>
+						<li class="menu-unit">Услуги</li>
+						<li class="menu-unit">Отзывы</li>
+						<li class="menu-unit">Вопросы и ответы</li>
+						<li class="menu-unit">Статьи</li>
+						<li class="menu-unit">Информация</li>
+					</ul>
+				</div>
+			</div>
+			<!--Search block-->
+			<div class="col-lg-6 search"></div>
+			<div class="col-lg-6 contacts"></div>
+			<div class="col-lg-12 main-pic"></div>
+		</div>
 </body>
 </html>
