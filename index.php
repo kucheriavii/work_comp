278ed3619bb801589578aafc6a1a82630da483de
kucheriavii <kucheriavii@gmail.com>
2015-08-23 22:03:10 +0300

    project start

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>test work</title>
+	<link rel="stylesheet" href="main.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+</head>
+<body>
+	<!--BOOTSTRAP INIT-->
+	<div class="container-fluid">
+
+	</div>	
+</body>
+</html>
